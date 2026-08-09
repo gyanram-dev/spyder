c9a979fa6e7fc5acbe83d37d40fd3580c5b81aa3
Gyan Ram <gyanramgrd1@gmail.com>
2026-08-09 18:13:48 +0530

    release: TimeBound v0.1.5

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -82,6 +82,14 @@ fn hide_reminder(app: tauri::AppHandle) {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init(|app, args, _cwd| {
+            println!("[single-instance] Second launch attempt detected with args: {:?}. Focusing main window.", args);
+            if let Some(window) = app.get_webview_window("main") {
+                let _ = window.show();
+                let _ = window.unminimize();
+                let _ = window.set_focus();
+            }
+        }))
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_autostart::init(
